01aab2ee49a009e82517b5d500c3ac36885ef8c6
PotapenkoS <wrunkiev@ukr.net>
2019-07-04 09:35:32 +0300

    lesson28

---
+++ src/lesson28/DemoComparator.java
@@ -0,0 +1,38 @@
+package lesson28;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+
+public class DemoComparator {
+    public static void main(String[] args) {
+        Capability capability1 = new Capability(1001, "A", "rrrr", true, new Date());
+        Capability capability2 = new Capability(1005, "B", "rrrr", false, new Date());
+        Capability capability3 = new Capability(900, "C", "rrrr", true, new Date());
+        Capability capability4 = new Capability(900, "D", "rrrr", false, new Date());
+
+        ArrayList<Capability> capabilities = new ArrayList<>();
+        capabilities.add(capability2);
+        capabilities.add(capability1);
+        capabilities.add(capability4);
+        capabilities.add(capability3);
+
+        System.out.println(capabilities);
+        capabilities.sort(new FullComparator());
+        System.out.println(capabilities);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/lesson28/FullComparator.java
@@ -0,0 +1,17 @@
+package lesson28;
+
+import java.util.Comparator;
+
+public class FullComparator implements Comparator<Capability> {
+    @Override
+    public int compare(Capability o1, Capability o2) {
+        if(o1.getChannelName() != null && o2.getChannelName() != null && !o1.getChannelName().equals(o2.getChannelName()))
+            return o1.getChannelName().compareTo(o2.getChannelName());
+        else if(o1.getFingerprint() != null && o2.getFingerprint() != null && !o1.getFingerprint().equals(o2.getFingerprint()))
+            return o1.getFingerprint().compareTo(o2.getFingerprint());
+        else if(o1.getDateCreated() != null && o2.getDateCreated() != null && !o1.getDateCreated().equals(o2.getDateCreated()))
+            return o2.getDateCreated().compareTo(o1.getDateCreated());
+        else
+            return 0;
+    }
+}
