222fb218e9fd85d8b32208cdba63512fd0e9afc5
PotapenkoS <wrunkiev@ukr.net>
2019-07-24 15:26:56 +0300

    lesson34.homework3

---
+++ src/lesson34/homework3/Demo.java
@@ -0,0 +1,8 @@
+package lesson34.homework3;
+
+public class Demo {
+    public static void main(String[] args)throws Exception {
+        //Solution.copyFileContent("C:/Soft/Files/fileFrom.txt", "C:/Soft/Files/fileTo.txt");
+        Solution.copyFileContentApacheIO("C:/Soft/Files/fileFrom.txt", "C:/Soft/Files/fileTo.txt");
+    }
+}
